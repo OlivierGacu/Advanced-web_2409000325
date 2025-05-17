4b3ac8eb8c71c84fd0b5344cb2ad173fde9a27b2
OlivierGacu <resistancebandzs@gmail.com>
2025-05-17 11:42:12 +0200

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,6 @@
+prices = {"Apple": 1000, "Banana": 500, "Mango": 1200}
+total_cost = 0
+for fruit, price in prices.items():
+    print(f"{fruit}: {price} RWF per kg")
+    total_cost += price * 2  
+print(f"Total cost for 2kg of each fruit: {total_cost} RWF")
